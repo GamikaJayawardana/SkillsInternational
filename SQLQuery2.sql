41d140d33bc30fea6e7a677a9709bd3c04495e69
Gamika Jayawardana <gamikakj@gmail.com>
2026-01-09 00:36:52 +0530

    Initial commit

---
+++ SQLQuery2.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Registration (
+    regNo int PRIMARY KEY,
+    firstName varchar(50),
+    lastName varchar(50),
+    dateOfBirth date,
+    gender varchar(50),
+    address varchar(50),
+    email varchar(50),
+    mobilePhone int,
+    homePhone int,
+    parentName varchar(50),
+    nic varchar(50),
+    contactNo int
+);
